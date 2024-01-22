2bf9c9cafbd4fd2f616ba75299a1597afc0c4e35
vtur <winndin@gmail.com>
2024-01-22 12:33:46 +0300

    add comment

--- src/models/car.ts
+++ src/models/car.ts
@@ -12,6 +12,6 @@ const carSchema: Schema<Car> = new Schema<Car>({
   name: { type: String, required: true, index: true },
   year: { type: Number, index: true },
   price: { type: Number, index: true },
-});
+});//brand index for search, others for faster sorting
 
 export const CarModel: Model<Car & Document> = model<Car & Document>('Car', carSchema);
